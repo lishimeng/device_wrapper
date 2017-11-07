df363da211027e246c32b82ae78e7a951b2c20ca
lishimeng <alex.li@jxfinancial.cn>
2017-11-07 13:39:28 +0800

    notification->vibrate

--- src/main/java/com/thingple/tagservice/device/AppNotify.java
+++ src/main/java/com/thingple/tagservice/device/AppNotify.java
@@ -1,49 +1,38 @@
 package com.thingple.tagservice.device;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.content.Context;
-import android.support.v4.app.NotificationCompat;
+import android.os.Vibrator;
+
+import static android.content.Context.VIBRATOR_SERVICE;
 
 /**
  *
  * Created by lism on 2017/8/15.
  */
-
-public class AppNotify {
-
-    private static int notifyId = 0;
+class AppNotify {
 
     private Context context;
 
-    NotificationManager notificationManager;
-
-    public AppNotify(Context context) {
+    AppNotify(Context context) {
         this.context = context;
         init();
     }
 
     private void init() {
-        notificationManager = (NotificationManager) this.context.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
-    public void start() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this.context);
-        Notification notification = builder.setWhen(System.currentTimeMillis())
-                .setContentTitle("Tag")
-                .setContentText("Inventory正在执行")
-                .setTicker("Inventory正在执行")
-                .setDefaults(Notification.DEFAULT_SOUND)
-                .setOngoing(true)
-                .setAutoCancel(true)
-                .build();
-        notification.flags |= Notification.FLAG_INSISTENT;
-
-        this.notificationManager.notify(notifyId, notification);
-
+    void start() {
+        long[] pattern = {1000, 400, 1000, 400};
+        Vibrator vibrator = (Vibrator) context.getSystemService(VIBRATOR_SERVICE);
+        if (vibrator != null) {
+            vibrator.vibrate(pattern, 0);
+        }
     }
 
-    public void destroy() {
-        notificationManager.cancel(notifyId);
+    void destroy() {
+        Vibrator vibrator = (Vibrator) context.getSystemService(VIBRATOR_SERVICE);
+        if (vibrator != null) {
+            vibrator.cancel();
+        }
     }
 }
